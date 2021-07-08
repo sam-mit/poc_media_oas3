6d2876a916ff5e24e8d9d3ffa6397013efc3a0e8
soumits <soumit.saha@vodafone.com>
2021-07-08 11:30:57 +0100

    Initial commit

---
+++ src/main/java/com/poc/media/poc_media_oas3/PocMediaOas3Application.java
@@ -0,0 +1,13 @@
+package com.poc.media.poc_media_oas3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMediaOas3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PocMediaOas3Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/poc/media/poc_media_oas3/PocMediaOas3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.media.poc_media_oas3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMediaOas3ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
